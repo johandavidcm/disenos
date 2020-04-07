de571da7bea1716881c93778494d69144156e81c
Johan David <johancarras16@gmail.com>
2020-04-06 21:18:10 -0500

    Proyecto Seccion Diseños

---
+++ lib/src/pages/basico_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+          _crearImagen(),
+          _crearTitulo(),
+          _acciones(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+        ],
+        ),
+      ),
+    ));
+  }
+
+  Widget _crearTitulo() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Billie Eilish',
+                    style: estiloTitulo,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    'Cantante',
+                    style: estiloSubtitulo,
+                  ),
+                ],
+              ),
+            ),
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text(
+              '10',
+              style: TextStyle(fontSize: 20.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAIACTm.img?h=0&w=720&m=6&q=60&u=t&o=f&l=f'),
+            fit: BoxFit.cover,
+            height: 300.0,
+      ),
+    );
+  }
+
+  Widget _acciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'Llamar'),
+        _accion(Icons.near_me, 'Ruta'),
+        _accion(Icons.share, 'Compartir')
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, color: Colors.blue, size: 40.0,), 
+        SizedBox(height: 5.0,),
+        Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue),)
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+        child: Text(
+          'Quis Lorem reprehenderit aute laboris laboris. Aute irure est cupidatat officia sunt nostrud cillum voluptate duis officia magna et laborum nostrud. Consectetur qui in commodo cupidatat irure proident proident minim occaecat fugiat esse voluptate. Nulla exercitation non nostrud quis nisi nostrud ullamco esse enim et dolor amet exercitation ipsum. Commodo ex laborum cupidatat occaecat non exercitation Lorem culpa do id. Aliqua magna cupidatat velit dolor esse cillum culpa ad dolore aliqua labore. Est anim non mollit ex.',
+          textAlign: TextAlign.justify,
+        ),
+      ),
+    );
+  }
+}
